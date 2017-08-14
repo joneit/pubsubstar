f9abc85298217430988a696d0d5db430dbb31c76
Jonathan Eiten <jonathan@openfin.co>
2017-08-14 10:26:45 -0400

    complete 1.0.0

---
+++ index.js
@@ -0,0 +1,157 @@
+/**
+ * pubsubstar
+ * https://github.com/joneit/pubsubstar.git
+ * Created by joneit on 8/13/17.
+ */
+
+/**
+ * The `subscriptions` hash is created on each context:
+ * 1. This object can serve as a context (`pubsubstar.subscribe(...)`, _etc._); or
+ * 2. Mix this object into your own object to use your own object's context; or
+ * 3. Call each method with `.call` to specify a context.
+ *
+ * @module
+ * @name pubsubstar
+ *
+ */
+
+
+'use strict';
+
+
+module.exports = {
+
+    /**
+     * @name subscribe
+     * @memberOf module:pubsubstar
+     * @summary Binds `subscriber` to `topic`.
+     * @desc Multiple subscribers may be bound in consecutive calls.
+     * All bound subscribers will be called when {@link module:pubsubstar#publish} is called with the same topic string.
+     * @param {string} topic - Topic to subscribe `subscriber` to.
+     * @param {function} subscriber
+     */
+    subscribe: function (topic, subscriber) {
+        if (typeof topic !== 'string') {
+            throw new TypeError('Expected topic to be a string.');
+        }
+
+        if (typeof subscriber !== 'function') {
+            throw new TypeError('Expected subscriber to be a function.');
+        }
+
+        var subscriptions = this.subscriptions = this.subscriptions || Object.create(null),
+            subscribers = subscriptions[topic] = subscriptions[topic] || [],
+            subscriberNotFound = subscribers.indexOf(subscriber) < 0;
+
+        if (subscriberNotFound) {
+            subscribers.push(subscriber);
+        }
+    },
+
+    /**
+     * Unsubscribe `subscriber` (or all subscribers) from `topic` (or from all topics).
+     * @param {string|RegExp} topic - Topic to unsubscribe `subscriber` from.
+     * To match multiple topics, include `*` wildcard(s) or specify a `RegExp`.
+     * It is recommended that your regex patterns begin with `^` and end with `$` to match whole topic strings.
+     * @param {function} [subscriber] - If not given, unsubscribes all subscribers from all specified `topics`.
+     */
+    unsubscribe: function (topics, subscriber) {
+        forEachTopic.call(this, topics, function (subscribers, topic, subscriptions) {
+            if (subscriber) {
+                var subscriberIndex = subscribers.indexOf(subscriber),
+                    subscriberFound = subscriberIndex >= 0;
+
+                if (subscriberFound) {
+                    subscribers.splice(subscriberIndex, 1); // remove subscriber from each topic wherein found
+                }
+            } else {
+                delete subscriptions[topic]; // unsubscribe all subscribers from each topic
+            }
+        });
+    },
+
+    /**
+     * Publishes `message` to the given `topic`, invoking any subscribers bound to `topic` by {@link DataSourceBase#subscribe}.
+     *
+     * ### Results
+     * @example
+     * dataModel.publish('set-sorts', sorts); // results discarded
+     * @example <caption>Synchronous subscribers returning values</caption>
+     * var results = dataModel.publish('get-sorts'); // array of results from all subscribers
+     * @example <caption>Asynchronous subscribers (return promises)</caption>
+     * Promise.all(publish('get-sorts')).then(function(resultsArray) { ... });
+     * @param {string|RegExp} topic - Topic to publish to.
+     * To match multiple topics, include `*` wildcard(s) or specify a `RegExp`.
+     * @param {*} [message]
+     * @returns {Array} Results of calls to all bound subscribers.
+     *
+     * ### About returned results
+     *
+     * Any topic can return useful information but in practice only certain topics do so.
+     * These are typically named starting with `get-`.
+     *
+     * Results are always returned in an array, one result from each subscriber.
+     * The length of the returned array represents the number of subscribers called.
+     * Typically there will be a single subscriber, in which case `publish` will return a single-element array.
+     * When there are multiple subscribers, the array will hold multiple results.
+     * Although an array, the order of the results should be considered undefined;
+     * if you need to know which of several subscribers generated a particular
+     * result, the subscriber should put identifying information in the result.
+     *
+     * ### Asynchronous subscribers
+     * If your subscribers are asynch, a very useful pattern is to have them return promises. `publish` will then return
+     * an array of promises, which in turn can be used to determine when all the subscribers have responded by calling
+     * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all Promise.all}.
+     * For example, to wait for all the subscribers to complete and obtain the final results array:
+     * ```js
+     * Promise.all(publish(topic, data))
+     *     .catch(function(reason) {
+     *         // If you get here it means at least one of the subscribers failed,
+     *         // having called `reject` or having thrown an error.
+     *     })
+     *     .then(function(resultsArray) {
+     *         // If you get here it means all the promises succeeded, all having called `resolve` with some value.
+     *         // The results (i.e., all the resolve values) are in the parameter which is an array.
+     *     });
+     * ```
+     * Caveat: Only call Promise.all on the results when the subscribers return promises.
+     * Although these would typically be async subscribers, they can include synchronous subscribers as well so long as
+     * they too return promises, although in those cases they would be pre-resolved promises (see
+     * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve Promise.resolve()}).
+     * This pattern accommodates a mixture of sync and async subscribers all subscribed to the same topic.
+     */
+    publish: function(topics, message) {
+        var results = [];
+
+        forEachTopic.call(this, topics, function(subscribers, topic, subscriptions) {
+            for (var i = 0; i < subscribers.length; ++i) {
+                results.push(subscribers[i].call(this, message));
+            }
+        });
+
+        return results;
+    }
+
+};
+
+
+function forEachTopic(topics, fn) {
+    if (!this.subscriptions) {
+        this.subscriptions = Object.create(null);
+        return;
+    }
+
+    // make sure `topics` is a regex
+    if (!(topics instanceof RegExp)) {
+        if (typeof topics !== 'string') {
+            throw new TypeError('Expected topic to be a string (with optional "*" wildcards) or a regex.');
+        }
+        topics = new RegExp('^' + topics.replace(/\*/g, '.*') + '$');
+    }
+
+    for (var topic in this.subscriptions) {
+        if (topics.test(topic) && this.subscriptions[topic]) {
+            fn.call(this, this.subscriptions[topic], topic, this.subscriptions);
+        }
+    }
+}
---
+++ test.js
@@ -0,0 +1,228 @@
+'use strict';
+
+var should = require('should');
+var pubsubstar = require('.');
+
+describe('require("pubsubstar")', function() {
+    it('is an object', function() {
+        pubsubstar.should.be.an.Object();
+    });
+    describe('.subscribe', function() {
+        var called = 0;
+        it('is a function', function() {
+            pubsubstar.should.have.a.property('subscribe').which.is.a.Function();
+        });
+        it('takes 2 parameters', function() {
+            pubsubstar.subscribe.length.should.equal(2);
+        });
+        it('throws a TypeError when called with 1st parameter of wrong type', function() {
+            should.throws(function() { pubsubstar.subscribe(3, function() {}); }, TypeError);
+        });
+        it('throws a TypeError when called with 2nd parameter of wrong type', function() {
+            should.throws(function() { pubsubstar.subscribe('a', 3); }, TypeError);
+        });
+        it('does not throw a TypeError when called with both parameters of the expected types', function() {
+            should.doesNotThrow(function() { pubsubstar.subscribe('a', function() {}) }, TypeError);
+        });
+    });
+    describe('.publish', function() {
+        it('is a function', function() {
+            pubsubstar.should.have.a.property('publish').which.is.a.Function();
+        });
+        it('takes up to 2 parameters', function() {
+            pubsubstar.publish.length.should.equal(2);
+        });
+        it('throws a TypeError when called with 1st parameter of wrong type', function() {
+            should.throws(function() { pubsubstar.publish(3); }, TypeError);
+        });
+        it('does not throw a TypeError when called with 1st parameter of type string', function() {
+            should.doesNotThrow(function() { pubsubstar.publish('a') }, TypeError);
+        });
+        it('does not throw a TypeError when called with 1st parameter of type RegExp', function() {
+            should.doesNotThrow(function() { pubsubstar.publish(/a/) }, TypeError);
+        });
+        it('does not call a subscriber previously subscribed to different topic, returning empty array', function() {
+            var called = 0;
+            function spy() { called++; }
+            pubsubstar.subscribe('b', spy);
+            pubsubstar.publish('c').should.be.Array().and.have.length(0);
+            called.should.equal(0);
+        });
+        it('calls a subscriber previously subscribed to same topic, returning 1-element array', function() {
+            var called = 0;
+            function spy() { called++; }
+            pubsubstar.subscribe('c', spy);
+            pubsubstar.publish('c').should.be.Array().and.have.length(1);
+            called.should.equal(1);
+        });
+        it('calls subscriber with message', function() {
+            var calledWith;
+            var MESSAGE = 'yada';
+            function spy(message) { calledWith = MESSAGE; }
+            pubsubstar.subscribe('d', spy);
+            pubsubstar.publish('d', MESSAGE);
+            calledWith.should.be.equal(MESSAGE);
+        });
+        it('returns subscriber\'s return value in first elemnt of array', function() {
+            var VALUE = 'yada';
+            function spy() { return VALUE; }
+            pubsubstar.subscribe('e', spy);
+            pubsubstar.publish('e')[0].should.be.equal(VALUE);
+        });
+        it('called with 2 subscribers, calls both', function() {
+            var n = 0;
+            pubsubstar.subscribe('f', function() { n += 10; });
+            pubsubstar.subscribe('f', function() { n += 1; });
+            pubsubstar.publish('f');
+            n.should.equal(11);
+        });
+        it('called with 2 subscribers, yields exactly those results', function() {
+            pubsubstar.subscribe('g', function() { return 17; });
+            pubsubstar.subscribe('g', function() { return 53; });
+            var result = pubsubstar.publish('g');
+            result.should.be.Array().and.have.length(2);
+            result.indexOf(17).should.be.aboveOrEqual(0);
+            result.indexOf(53).should.be.aboveOrEqual(0);
+        });
+        it('called topic "*" and 3 subscribers with varied topics, calls all three', function() {
+            var n = 0;
+            pubsubstar.subscribe('abe', function(m) { n += m * 100; });
+            pubsubstar.subscribe('ape', function(m) { n += m * 10; });
+            pubsubstar.subscribe('babe', function(m) { n += m * 1; });
+            pubsubstar.publish('*', 3);
+            n.should.equal(333);
+        });
+        it('called topic "a*e" and 3 subscribers with varied topics and 1 match, calls just that one', function() {
+            var n = 0;
+            pubsubstar.subscribe('abey', function(m) { n += m * 100; });
+            pubsubstar.subscribe('ape', function(m) { n += m * 10; });
+            pubsubstar.subscribe('babe', function(m) { n += m * 1; });
+            pubsubstar.publish('a*e', 3);
+            n.should.equal(30);
+        });
+        it('called topic "a*" and 3 subscribers with varied topics and 2 matches, calls just those 2', function() {
+            var n = 0;
+            pubsubstar.subscribe('abe', function(m) { n += m * 100; });
+            pubsubstar.subscribe('ape', function(m) { n += m * 10; });
+            pubsubstar.subscribe('babe', function(m) { n += m * 1; });
+            pubsubstar.publish('a*', 3);
+            n.should.equal(330);
+        });
+        it('called topic /a.e/ and 4 subscribers with varied topics and 3 matches, calls just those 3', function() {
+            var n = 0;
+            pubsubstar.subscribe('abe', function(m) { n += m * 100; });
+            pubsubstar.subscribe('ape', function(m) { n += m * 10; });
+            pubsubstar.subscribe('bike', function(m) { n += m * 1; });
+            pubsubstar.subscribe('babe', function(m) { n += m * 1000; });
+            pubsubstar.publish(/a.e/, 3);
+            n.should.equal(3330);
+        });
+        it('with async subscribers (return promises)', function() {
+            function spy1(message) { return new Promise(function(resolve) { setTimeout(function() { resolve(17); }, 500); }); }
+            function spy2(message) { return new Promise(function(resolve) { setTimeout(function() { resolve(34); }, 1000); }); }
+            pubsubstar.subscribe('h', spy1);
+            pubsubstar.subscribe('h', spy2);
+            return Promise.all(pubsubstar.publish('h')).should.eventually.containEql(17).and.containEql(34);
+        });
+    });
+    describe('.unsubscribe', function() {
+        it('is a function', function() {
+            pubsubstar.should.have.a.property('unsubscribe').which.is.a.Function();
+        });
+        it('takes 2 parameters', function() {
+            pubsubstar.unsubscribe.length.should.equal(2);
+        });
+        it('throws a TypeError when called with 1st parameter of wrong type', function() {
+            should.throws(function() { pubsubstar.publish(3); }, TypeError);
+        });
+        it('does not throw a TypeError when called with 1st parameter of type string', function() {
+            should.doesNotThrow(function() { pubsubstar.publish('a') }, TypeError);
+        });
+        it('does not throw a TypeError when called with 1st parameter of type RegExp', function() {
+            should.doesNotThrow(function() { pubsubstar.publish(/a/) }, TypeError);
+        });
+        it('unsubscribes specific subscriber previously subscribed to same topic', function() {
+            var called = 0;
+            function spy() { called++; }
+            pubsubstar.subscribe('a', spy);
+            pubsubstar.publish('a');
+            called.should.equal(1);
+            called = 0;
+            pubsubstar.unsubscribe('a', spy);
+            pubsubstar.publish('a');
+            called.should.equal(0);
+        });
+        it('does not unsbuscribe other subscriber previously subscribed to same topic', function() {
+            var called1 = 0, called2 = 0;
+            function spy1() { called1++; }
+            function spy2() { called2++; }
+            pubsubstar.subscribe('b', spy1);
+            pubsubstar.subscribe('b', spy2);
+            pubsubstar.publish('b');
+            called1.should.equal(1);
+            called2.should.equal(1);
+            called1 = called2 = 0;
+            pubsubstar.unsubscribe('b', spy1);
+            pubsubstar.publish('b');
+            called1.should.equal(0);
+            called2.should.equal(1);
+        });
+        it('unsubscribe sans 2nd param unsubscribes all subscribers previously subscribed to same topic', function() {
+            var called1 = 0, called2 = 0;
+            function spy1() { called1++; }
+            function spy2() { called2++; }
+            pubsubstar.subscribe('c', spy1);
+            pubsubstar.subscribe('c', spy2);
+            pubsubstar.publish('c');
+            called1.should.equal(1);
+            called2.should.equal(1);
+        });
+        it('unsubscribe sans 2nd param does not unsubscribe subscriber previously subscribed to different topic', function() {
+            var called1 = 0;
+            function spy1() { called1++; }
+            pubsubstar.subscribe('d', spy1);
+            pubsubstar.unsubscribe('c');
+            pubsubstar.publish('d');
+            called1.should.equal(1);
+        });
+        it('unsubscribe("*", specific) unsubscribes specific subscribers previously subscribed to various topics', function() {
+            var called = 0;
+            function spy() { called++; }
+            pubsubstar.subscribe('e', spy);
+            pubsubstar.subscribe('f', spy);
+            pubsubstar.unsubscribe('*', spy);
+            pubsubstar.publish('e');
+            pubsubstar.publish('f');
+            called.should.equal(0);
+        });
+        it('unsubscribe("*", specific) does not unsubscribe subscribers previously subscribed to various topics with different specificities', function() {
+            var called1 = 0, called2 = 0;
+            function spy1() { called1++; }
+            function spy2() { called2++; }
+            pubsubstar.subscribe('g', spy1);
+            pubsubstar.subscribe('g', spy2);
+            pubsubstar.subscribe('h', spy1);
+            pubsubstar.subscribe('h', spy2);
+            pubsubstar.unsubscribe('*', spy1);
+            pubsubstar.publish('g');
+            pubsubstar.publish('h');
+            called1.should.equal(0);
+            called2.should.equal(2);
+        });
+        it('unsubscribe("*") unsubscribes all subscribers previously subscribed to various topics', function() {
+            var called1 = 0, called2 = 0, called3 = 0;
+            function spy1() { called1++; }
+            function spy2() { called2++; }
+            function spy3() { called3++; }
+            pubsubstar.subscribe('i', spy1);
+            pubsubstar.subscribe('i', spy2);
+            pubsubstar.subscribe('j', spy3);
+            pubsubstar.unsubscribe('*');
+            pubsubstar.publish('i');
+            pubsubstar.publish('j');
+            called1.should.equal(0);
+            called2.should.equal(0);
+            called3.should.equal(0);
+        });
+    });
+});
